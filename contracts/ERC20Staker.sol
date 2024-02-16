c47c2f633deebebec07a75b557a46f0bf448204d
DeepSoul71 <levilee9571@gmail.com>
2024-02-17 04:25:10 +1000

    feat:staking completed

---
+++ contracts/ERC20Staker.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+ 
+pragma solidity ^0.8.20;
+
+interface IERC20 {
+
+  function totalSupply() external view returns (uint);
+
+  function balanceOf(address account) external view returns (uint);
+
+  function transfer(address recipient, uint amount) external returns (bool);
+
+  function allowance(address owner, address spender) external view returns (uint);
+
+  function approve(address spender, uint amount) external returns (bool);
+
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint amount
+  ) external returns (bool);
+
+
+  function mint(address to, uint256 amount) external;
+
+  event Transfer(address indexed from, address indexed to, uint value);
+
+  event Approval(address indexed owner, address indexed spender, uint value);
+
+}
+
+
+contract ERC20Staker {
+  IERC20 public tokenToStake;
+  IERC20 public rewardToken;
+  mapping(address => uint256) public stakedAmount;
+  mapping(address => uint256) public lastStakeTime;
+
+  event Staked(address indexed _staker, uint256 amount);
+  event Withdrawn(address indexed _staker, uint256 amount);
+  event RewardPaid(address indexed _staker, uint256 amount);
+
+  constructor(address _tokenToStake, address _rewardToken) {
+    tokenToStake = IERC20(_tokenToStake);
+    rewardToken = IERC20(_rewardToken);
+  }
+
+  function stake(uint256 _amount) external {
+    require(_amount > 0, "Staking amount must be greater than 0");
+    require(tokenToStake.balanceOf(msg.sender) >= _amount, "Insufficient balance");
+    require(tokenToStake.allowance(msg.sender, address(this)) >= _amount, "Insufficient allowance");
+    if(stakedAmount[msg.sender] == 0) {
+      lastStakeTime[msg.sender] = block.timestamp;
+    }
+
+    tokenToStake.transferFrom(msg.sender, address(this), _amount);
+    stakedAmount[msg.sender] += _amount;
+  }
+
+  function withdraw(uint256 amount) external {
+    require(amount > 0, "Withdrawal amount must be greater than 0");
+    require(stakedAmount[msg.sender] >= amount, "Insufficient staked amount");
+
+    uint256 reward = getReward(msg.sender);
+    if (reward > 0) {
+      rewardToken.mint(msg.sender, reward);
+      emit RewardPaid(msg.sender, reward);
+    }
+
+    stakedAmount[msg.sender] -= amount;
+    tokenToStake.transfer(msg.sender, amount);
+    emit Withdrawn(msg.sender, amount);
+  }
+
+  function getReward(address user) public view returns (uint256) {
+    uint256 timeElapsed = block.timestamp - lastStakeTime[user];
+    uint256 stakedAmountUser = stakedAmount[user];
+    if (timeElapsed == 0 || stakedAmountUser == 0) {
+        return 0;
+    }
+    uint256 reward = stakedAmountUser * timeElapsed;
+    return reward;
+  }
+
+  function getStakedBalance(address user) public view returns (uint256) {
+    return stakedAmount[user];
+  }
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,27 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: process.env.NEXT_APP_ALCHEMI_API_URL,
+      accounts: [ process.env.NEXT_APP_RIVATE_KEY ]
+    }
+  },
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
 };
 
 export default config;
